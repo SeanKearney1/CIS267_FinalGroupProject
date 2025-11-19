e70a9fccd33d83783954630e4ddb8b4ff1cc1dc0
Julian <jmiles5@my.monroeccc.edu>
2025-11-19 18:29:40 -0500

    fixed placement system

--- Assets/Scripts/Building/TilePlacment.cs
+++ Assets/Scripts/Building/TilePlacment.cs
@@ -8,11 +8,15 @@ using static UnityEditor.FilePathAttribute;
 public class TilePlacment : MonoBehaviour
 {
     
-    public Tilemap tileMapToPlaceOn;
+    public Tilemap tileMap;
+    public TileBase towerTile;//replaces the tile on the tilemaps with a tower tile, this has no visual effect as the physical scripted tower will be placed above it
+    public TileBase grassTile;//The tile it checks the tile map for and allows placement only on it
+
     // this takes the tiles in the tile folder with the purple icon to their side
 
-    public TileBase[] tileToPlace;// the first in this array is selection = 0, ++0 for each subsequent selection
-    private int selection;// this is the  selection of tileToPlace[selection]
+    public GameObject[] TowerToPlace;// the first in this array is selection = 0, ++0 for each subsequent selection
+
+    private int selection=0;// this is the  selection of tileToPlace[selection]
     private void Update()
     {
         placeTileOnMousePoint();
@@ -26,15 +30,29 @@ public class TilePlacment : MonoBehaviour
             // get the position of the cursor
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             // convert the cursor location to a grid location
-            Vector3Int cellPos = tileMapToPlaceOn.WorldToCell(mousePos);
+            Vector3Int cellPos = tileMap.WorldToCell(mousePos);
 
-            //place the tile on the grid
-            tileMapToPlaceOn.SetTile(cellPos, tileToPlace[selection]);
+            placeTower(cellPos);
+
+
+
         }
     }
+    private void placeTower(Vector3Int cellPos)
+    {
+        TileBase tile = tileMap.GetTile(cellPos);
+        Vector3 placePos = tileMap.GetCellCenterWorld(cellPos);//get centre of the cell
+        if (tile == grassTile)// if the selected tile can be placed on
+        {
+            tileMap.SetTile(cellPos, towerTile);
+            Instantiate(TowerToPlace[selection], placePos, Quaternion.identity);//create the physical tile
+        }
+        
+    }
+
     public void switchSelection(int index)//probably place this on ui buttons onclick
-    {// go to button in onclick() hit plus, drag the object with this script in, to the left of that select this function and type in a index number
-        if (index >= 0 && index < tileToPlace.Length)
+    {
+        if (index >= 0 && index < TowerToPlace.Length)
             selection = index;
         else
             Debug.LogWarning("Invalid tile index!");
